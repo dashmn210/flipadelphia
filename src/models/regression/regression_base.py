87a5fa60cbc50024e0c293f446d8f0dcb6d9134a
rpryzant <rapigan@gmail.com>
2017-10-11 16:52:15 -0700

    rm method

--- src/models/regression/regression_base.py
+++ src/models/regression/regression_base.py
@@ -125,48 +125,6 @@ class Regression:
 
 
 
-    def train(self, dataset, model_dir):
-        """ trains the model using a src.data.dataset.Dataset
-            saves model-specific metrics (loss, etc) into self.report
-        """
-        train_split = self.config.train_suffix
-
-        targets = [
-            variable for variable in self.config.data_spec[1:] \
-            if variable['control'] == False \
-            and not variable.get('skip', False)]
-        confounds = [
-            variable for variable in self.config.data_spec[1:] \
-            if variable['control'] \
-            and not variable.get('skip', False)] 
-
-        for i, target in enumerate(targets):
-            ignored_targets = targets[:i] + targets[i+1:]
-
-            if target['type']== 'continuous':
-                fitting_function = self._fit_mixed_regression
-            else:
-                fitting_function = self._fit_mixed_ovr
-
-            self.models[target['name']] = fitting_function(
-                split=train_split,
-                dataset=dataset,
-                target=target,
-                ignored_vars=ignored_targets,
-                confounds=confounds)
-
-
-
-
-
-
-
-
-
-
-
-
-
     def inference(self, dataset, model_dir, dev=True):
         """ run inference on the dev/test set, save all predictions to 
                 per-variable files in model_dir, and return pointers to those files
